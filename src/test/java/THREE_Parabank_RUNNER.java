0b208020744eb53d2722c6f6d0af01fc3d8a55bf
vask070707 <129285704+vask070707@users.noreply.github.com>
2023-03-30 20:14:28 +0100

    Update THREE_Parabank_RUNNER.java

--- src/test/java/THREE_Parabank_RUNNER.java
+++ src/test/java/THREE_Parabank_RUNNER.java
@@ -1,7 +1,7 @@
-// reports will be in the location
+// SHARVANI LEAD UPDATES - reports will be in the BELOW location
 // C:\Users\DELL\eclipse-workspace\org.octtestingbatch.vas\reports
-//Difference: implements means you are using the elements of a Java Interface in your class. extends means that you are creating a subclass of the base class you are extending.
-//// basically it maps stepdefintion with feature file with same wording Given etc, if any duplicate of same step definiton it throwserror. 
+// Extends means that you are creating a subclass of the base class you are extending.
+// basically it maps stepdefintion with feature file with same wording Given etc, if any duplicate of same step definiton it throwserror. 
 
 
 
@@ -11,8 +11,8 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features ="C:\\Users\\DELL\\eclipse-workspace\\org.octtestingbatch.vas\\src\\test\\resources\\Features\\ONE_Parabank1.feature",
 glue="parabankpackage",
-dryRun=false,   //  Dry-run is used to compile feature files and step definitions in cucumber. It is specially used in the stage when you will have to see if there are any compilation errors, to check that you can use dry-run. 
-monochrome = false,
+dryRun=true,   //  Dry-run is used to compile feature files and step definitions in cucumber. It is specially used in the stage when you will have to see if there are any compilation errors, to check that you can use dry-run. 
+monochrome = true,
 plugin = {"json:reports/jsonreport.json" ,"html:reports/cucumber-reports.html"}
 )
 
